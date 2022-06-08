9d5918231a5972fa8354609a47e9a1c43300e3f7
Reza Alizadeh Majd <r.a.majd@gmail.com>
2022-06-08 19:26:46 +0430

    aoc2015: day1-2

--- aoc/aoc2015/day01/src/main.rs
+++ aoc/aoc2015/day01/src/main.rs
@@ -1,16 +1,31 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+fn instruction_weight(ch: char) -> i32 {
+    match ch {
+        '(' => 1,
+        ')' => -1,
+        _ => 0,
+    }
+}
+
 fn calc_floor(input: &String) -> i32 {
     let mut floor = 0;
     for ch in input.chars() {
-        floor += match ch {
-            '(' => 1,
-            ')' => -1,
-            _ => 0,
-        }
+        floor += instruction_weight(ch);
     }
     floor
+}
+
+fn find_basement_position(input: &String) -> Option<usize> {
+    let mut position = 0;
+    for (idx, ch) in input.chars().enumerate() {
+        position += instruction_weight(ch);
+        if position == -1 {
+            return Some(idx + 1);
+        }
+    }
+    None
 }
 
 fn main() {
@@ -19,4 +34,10 @@ fn main() {
     file.read_to_string(&mut input).unwrap();
     let result = calc_floor(&input);
     println!("result = {}", result);
+
+    if let Some(basement_position) = find_basement_position(&input) {
+        println!("Basement position = {}", basement_position);
+    } else {
+        println!("Basement not found");
+    }
 }
