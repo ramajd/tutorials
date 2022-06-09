348c61ebf1e44c9f0a5cf504822e6dd6a544f3ac
Reza Alizadeh Majd <r.a.majd@gmail.com>
2022-06-09 18:10:34 +0430

    aoc2015: day2-2

--- aoc/aoc2015/day02/src/main.rs
+++ aoc/aoc2015/day02/src/main.rs
@@ -28,22 +28,40 @@ impl GiftBox {
         return 2 * s1 + 2 * s2 + 2 * s3;
     }
 
-    fn slack(&self) -> usize {
+    fn min_dimensions(&self) -> (usize, usize) {
         let max = self.dimensions.into_iter().max().unwrap();
 
         let result = if self.dimensions[0] == max {
-            self.dimensions[1] * self.dimensions[2]
+            (self.dimensions[1], self.dimensions[2])
         } else if self.dimensions[1] == max {
-            self.dimensions[0] * self.dimensions[2]
+            (self.dimensions[0], self.dimensions[2])
         } else {
             /* self.dimensions[2] == max */
-            self.dimensions[0] * self.dimensions[1]
+            (self.dimensions[0], self.dimensions[1])
         };
         result
     }
 
+    fn slack(&self) -> usize {
+        let dims = self.min_dimensions();
+        return dims.0 * dims.1;
+    }
+
+    fn volume(&self) -> usize {
+        self.dimensions[0] * self.dimensions[1] * self.dimensions[2]
+    }
+
+    fn shortest_perimeter(&self) -> usize {
+        let dims = self.min_dimensions();
+        return 2 * (dims.0 + dims.1);
+    }
+
     pub fn paper_size(&self) -> usize {
         return self.surface() + self.slack();
+    }
+
+    pub fn ribbon_length(&self) -> usize {
+        return self.shortest_perimeter() + self.volume();
     }
 }
 
@@ -51,15 +69,18 @@ fn main() {
     let file = File::open("input.txt").expect("Failed to read input");
     let buffer = BufReader::new(file);
 
-    let mut total_size = 0;
+    let mut total_papaer = 0;
+    let mut total_ribbon = 0;
 
     for line in buffer.lines() {
         if let Ok(line) = line {
             if let Some(gift) = GiftBox::parse(&line) {
-                total_size += gift.paper_size();
+                total_papaer += gift.paper_size();
+                total_ribbon += gift.ribbon_length();
             }
         }
     }
 
-    println!("total size: {}", total_size);
+    println!("total paper surface: {}", total_papaer);
+    println!("total ribbon length: {}", total_ribbon);
 }
