ee4cc8b55eb4ba282414050df5bea8562f70634e
Reza Alizadeh Majd <r.a.majd@gmail.com>
2022-06-12 09:26:53 +0430

    aoc2015: day4-2 - using threads

--- aoc/aoc2015/day04/src/main.rs
+++ aoc/aoc2015/day04/src/main.rs
@@ -1,3 +1,5 @@
+use std::{ops::Range, thread};
+
 use md5;
 
 fn calculate_hash(secret: &str, number: usize) -> String {
@@ -9,18 +11,44 @@ fn calculate_hash(secret: &str, number: usize) -> String {
     hash_str
 }
 
-fn mine_block(secret: &str, predict: &str) -> (usize, String) {
-    let mut number = 0;
-    let mut hash = calculate_hash(secret, number);
-    while !hash.starts_with(predict) {
-        number += 1;
-        hash = calculate_hash(secret, number);
+fn process_range(secret: &str, predict: &str, range: Range<usize>) -> Option<(usize, String)> {
+    for i in range {
+        let hash = calculate_hash(secret, i);
+        if hash.starts_with(predict) {
+            return Some((i, hash));
+        }        
     }
-    (number, hash)
+    None
+}
+
+fn mine_block(secret: &'static str, predict: &'static str) -> (usize, String) {
+    let cpu_count = num_cpus::get();
+    let step = 100_000;
+
+    let mut counter = 0;
+
+    loop {
+        let mut handles = Vec::new();
+        for _ in 0..cpu_count {
+            let range = counter..counter + step;
+
+            counter += step;
+            let handle = thread::spawn(move || process_range(secret, predict, range));
+            handles.push(handle);
+        }
+        for handle in handles {
+            if let Some((number, hash)) = handle.join().unwrap() {
+                return (number, hash);
+            }
+        }
+    }
 }
 
 fn main() {
     let secret = "iwrupvqb";
-    let result = mine_block(secret, "00000");
-    println!("{} - {}", result.0, result.1);
+    let part1 = mine_block(secret, "00000");
+    println!("Part1: {} - {}", part1.0, part1.1);
+
+    let part2 = mine_block(secret, "000000");
+    println!("Part2: {} - {}", part2.0, part2.1);
 }
