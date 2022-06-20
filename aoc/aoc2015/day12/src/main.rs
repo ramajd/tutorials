0371c92b4dd69459f3caaf57beeb93ef8ef442dd
Reza Alizadeh Majd <r.a.majd@gmail.com>
2022-06-20 15:38:05 +0430

    aoc2015: day12-2

--- aoc/aoc2015/day12/src/main.rs
+++ aoc/aoc2015/day12/src/main.rs
@@ -8,20 +8,31 @@ fn main() {
     let data: serde_json::Value =
         serde_json::from_reader(buffer).expect("Failed to read JSON file");
 
-    let sum = calculate_sum(&data);
+    let sum = calculate_sum(&data, &Vec::new());
     println!("Sum of all numbers = {}", sum);
+
+    let ignore_list = vec!["red"];
+    let sum = calculate_sum(&data, &ignore_list);
+    println!("Sum of the numbers ignoring {:?} = {}", ignore_list, &sum);
 }
 
-fn calculate_sum(data: &serde_json::Value) -> i64 {
+fn calculate_sum(data: &serde_json::Value, ignore_words: &Vec<&str>) -> i64 {
     let mut result = 0;
     if data.is_array() {
         for v in data.as_array().unwrap() {
-            result += calculate_sum(v);
+            result += calculate_sum(v, ignore_words);
         }
     } else if data.is_object() {
+        let mut obj_value = 0;
         for (_, v) in data.as_object().unwrap() {
-            result += calculate_sum(v);
+            if let Some(s) = v.as_str() {
+                if ignore_words.contains(&s) {
+                    return 0;
+                }
+            }
+            obj_value += calculate_sum(v, ignore_words);
         }
+        result += obj_value;
     } else if data.is_number() {
         result += data.as_i64().unwrap();
     }
@@ -35,13 +46,27 @@ mod test {
 
     #[test]
     fn test_calculate_sum() {
-        assert!(calculate_sum(&serde_json::json!([1, 2, 3])) == 6);
-        assert!(calculate_sum(&serde_json::json!({"a":2,"b":4})) == 6);
-        assert!(calculate_sum(&serde_json::json!([[[3]]])) == 3);
-        assert!(calculate_sum(&serde_json::json!({"a":{"b":4},"c":-1})) == 3);
-        assert!(calculate_sum(&serde_json::json!({"a":[-1,1]})) == 0);
-        assert!(calculate_sum(&serde_json::json!([-1,{"a":1}])) == 0);
-        assert!(calculate_sum(&serde_json::json!([])) == 0);
-        assert!(calculate_sum(&serde_json::json!({})) == 0);
+        assert!(calculate_sum(&serde_json::json!([1, 2, 3]), &Vec::new()) == 6);
+        assert!(calculate_sum(&serde_json::json!({"a":2,"b":4}), &Vec::new()) == 6);
+        assert!(calculate_sum(&serde_json::json!([[[3]]]), &Vec::new()) == 3);
+        assert!(calculate_sum(&serde_json::json!({"a":{"b":4},"c":-1}), &Vec::new()) == 3);
+        assert!(calculate_sum(&serde_json::json!({"a":[-1,1]}), &Vec::new()) == 0);
+        assert!(calculate_sum(&serde_json::json!([-1,{"a":1}]), &Vec::new()) == 0);
+        assert!(calculate_sum(&serde_json::json!([]), &Vec::new()) == 0);
+        assert!(calculate_sum(&serde_json::json!({}), &Vec::new()) == 0);
+    }
+
+    #[test]
+    fn test_calculate_ignoring_red() {
+        let ignore_list = vec!["red"];
+        assert!(calculate_sum(&serde_json::json!([1, 2, 3]), &ignore_list) == 6);
+        assert!(calculate_sum(&serde_json::json!([1,{"c":"red","b":2},3]), &ignore_list) == 4);
+        assert!(
+            calculate_sum(
+                &serde_json::json!({"d":"red","e":[1,2,3,4],"f":5}),
+                &ignore_list
+            ) == 0
+        );
+        assert!(calculate_sum(&serde_json::json!([1, "red", 5]), &ignore_list) == 6);
     }
 }
